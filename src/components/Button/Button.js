02a50eef0122da3ebc9d68d093cb2799df83dbc9
volkan <nzaro19@gmail.com>
2022-04-04 11:22:27 +0300

    button works

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,17 +2,11 @@ import React from 'react'
 
 import "./Button.css"
 
-const Button = () => {
+const Button = ({ children, className }) => {
 
   return (
-
-    <button
-      className="underline text-3xl text-blue-400 hover:text-blue-200 mt-4"
-      href="https://github.com/TheMagoo73/react-tailwind-storybook-example"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Text
+    <button class={className}>
+      {children}
     </button>
   )
 }
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,15 +3,29 @@ import Button from './Button';
 
 export default {
   title: 'Application/Component Library/Button',
-  component: Button,
+  component: 'Button',
   controls: {
     hideNoControlsWarning: true
-  }
+  },
+  argTypes: {
+    disabled: { control: 'boolean' },
+    children: { control: 'text' },
+  },
 }
 
-const Template = () => <Button />
+const Template = (args) => <Button {...args} />
 
-export const Default = Template.bind({})
-Default.parameters = {
-  controls: { hideNoControlsWarning: true }
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  children: "Click me",
+  className: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 }
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  children: "Disabled",
+  className: 'bg-blue-500 text-white font-bold py-2 px-4 rounded'
+}
+
